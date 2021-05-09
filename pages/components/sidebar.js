f4ee652c1713427b16a5eb3f3a97d7b504b223b3
andrentv <comercial@vaccaros.com.br>
2021-05-08 23:27:33 -0300

    first

---
+++ pages/components/sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className="w-96 shadow-md">
+            <div className="border flex">
+                <p>elrond</p>
+                <p>wallet</p>
+            </div>
+            <div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
